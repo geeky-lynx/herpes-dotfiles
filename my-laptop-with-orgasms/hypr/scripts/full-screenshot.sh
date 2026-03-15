4926c627830ff216a98d2acdc5eec412d8f72eda
geeky-lynx <markomacura003@gmail.com>
2026-03-15 11:47:35 +0100

    Update screenshot scripts to add screenshots to a clipboard

--- my-laptop-with-orgasms/hypr/scripts/full-screenshot.sh
+++ my-laptop-with-orgasms/hypr/scripts/full-screenshot.sh
@@ -3,11 +3,14 @@
 grim_dir="~/Pictures/grim-screenshots"
 
 grim -c "$HOME/Pictures/grim-screenshots/grim-$(date +%d_%m_%Y-%H:%M:%S-%s).png"
+
 if (( $? != 0 )) then
   hyprctl notify 5 4500 "rgb(f38ba8)" " Full Screenshot failed :("
   #                          ^^^^^^ catppuccin mocha red
   exit
 fi
+
+grim -c - | wl-copy # Copy to clipboard
 
 hyprctl notify 5 4500 "rgb(a6e3a1)" " Full Screenshot taken"
 #                          ^^^^^^ catppuccin mocha green
--- my-laptop-with-orgasms/hypr/scripts/region-screenshot.sh
+++ my-laptop-with-orgasms/hypr/scripts/region-screenshot.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/bash
 
 grim_dir="~/Pictures/grim-screenshots"
+dimensions=$(slurp -d)
 
-grim -g "$(slurp -d)" -c "$HOME/Pictures/grim-screenshots/grim-$(date +%d_%m_%Y-%H:%M:%S-%s).png"
+grim -c -g "$dimensions" "$HOME/Pictures/grim-screenshots/grim-$(date +%d_%m_%Y-%H:%M:%S-%s).png"
+
 if (( $? != 0 )) then
   hyprctl notify 5 4500 "rgb(f38ba8)" " Regional Screenshot failed :-."
   #                          ^^^^^^ catppuccin mocha red
   exit
 fi
+
+grim -c -g "$dimensions" - | wl-copy # Add to clipboard
 
 hyprctl notify 5 4500 "rgb(a6e3a1)" " Regional Screenshot taken"
 #                          ^^^^^^ catppuccin mocha green
